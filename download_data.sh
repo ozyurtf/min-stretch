ce29321eebe20d0ec001dee3ddc788b6949171b6
Haritheja Etukuru <hre7290@nyu.edu>
2024-11-16 18:46:17 -0500

    remove file id

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GDRIVE_FOLDER_ID="1VdQ-4U-EDwcwF1rwTv3o0O-Dk_nLZc9a"
+GDRIVE_FOLDER_ID="file_id" # make the zip file public and copy the file_id in the url. example url: https://drive.google.com/file/d/{FILE_ID}/view?usp=share_link
 TASK_NUMBER=1 # 1=Door_Opening 2=Door_Closing 3=Drawer_Opening 4=Drawer_Closing
 NAME="john" # enter first name; used to keep track and properly attribute data
 ENV_NO=1 # used to name and separate environments. starting with 1, increment by 1 for each new environment.

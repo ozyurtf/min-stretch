fef8f61d284f1b802b6d955757142a3e8efdf5c1
Haritheja Etukuru <hre7290@nyu.edu>
2024-11-19 13:52:10 -0500

    train command mod

--- test_vqbet_cpu.sh
+++ test_vqbet_cpu.sh
@@ -3,4 +3,4 @@
 include_task="Door_Opening"
 
 cd imitation-in-homes
-HYDRA_FULL_ERROR=1 accelerate launch --config_file configs/accelerate/accel_cfg.yaml train.py --config-name=train_vqbet.yaml include_task=$include_task vqvae_load_dir=null
+python train.py --config-name=train_vqbet.yaml include_task=$include_task vqvae_load_dir=null

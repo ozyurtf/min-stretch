8a9943453b14c34b27db6317b977b7fe62c91793
Haritheja Etukuru <hre7290@nyu.edu>
2024-11-19 14:44:43 -0500

    disable wandb

--- train_vqbet_model.sh
+++ train_vqbet_model.sh
@@ -12,4 +12,4 @@ cd imitation-in-homes
 HYDRA_FULL_ERROR=1 python train.py --config-name=train_rvq.yaml include_task=$include_task model_path=$RVQ_PATH
 sleep 10
 
-HYDRA_FULL_ERROR=1 accelerate launch --config_file configs/accelerate/accel_cfg.yaml train.py --config-name=train_vqbet.yaml include_task=$include_task vqvae_load_dir=$RVQ_PATH batch_size=$BATCH_SIZE
+HYDRA_FULL_ERROR=1 WANDB_MODE=disabled accelerate launch --config_file configs/accelerate/accel_cfg.yaml train.py --config-name=train_vqbet.yaml include_task=$include_task vqvae_load_dir=$RVQ_PATH batch_size=$BATCH_SIZE

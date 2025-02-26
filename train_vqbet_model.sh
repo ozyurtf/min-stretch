1f5e6f741a32385c469dbec411d1551bd31025d3
Haritheja Etukuru <hre7290@nyu.edu>
2025-02-26 11:48:16 -0500

    config changes

--- train_vqbet_model.sh
+++ train_vqbet_model.sh
@@ -12,7 +12,7 @@ wandb=true  # Set the wandb configuration here
 
 cd imitation-in-homes
 
-python train.py --config-name=train_rvq.yaml include_task=$include_task model_path=$RVQ_PATH
+WANDB_MODE=disabled python train.py --config-name=train_rvq.yaml include_task=$include_task model_path=$RVQ_PATH
 sleep 10
 
-WANDB_MODE=$( [ "$wandb" = true ] && echo "online" || echo "disabled" ) accelerate launch --config_file configs/accelerate/accel_cfg.yaml train.py --config-name=train_vqbet.yaml include_task=$include_task vqvae_load_dir=$RVQ_PATH batch_size=$BATCH_SIZE
+WANDB_MODE=$( [ "$wandb" = true ] && echo "online" || echo "disabled" ) accelerate launch --config_file configs/accelerate/accel_cfg.yaml train.py --config-name=train_vqbet.yaml include_task=$include_task vqvae_load_dir=$RVQ_PATH/checkpoint.pt batch_size=$BATCH_SIZE

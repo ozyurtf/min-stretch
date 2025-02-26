588e75c3aed9e0acc174412d15df4208023c1aac
Haritheja Etukuru <hre7290@nyu.edu>
2025-02-25 23:55:43 -0500

    wandb config

--- test_rvq_cpu.sh
+++ test_rvq_cpu.sh
@@ -2,5 +2,8 @@
 
 include_task="Door_Opening"
 
+wandb=true  # Set the wandb configuration here
+
 cd imitation-in-homes
-WANDB_MODE=disabled HYDRA_FULL_ERROR=1 python train.py --config-name=train_rvq include_task=$include_task device=cpu
+
+WANDB_MODE=$( [ "$wandb" = true ] && echo "online" || echo "disabled" ) HYDRA_FULL_ERROR=1 python train.py --config-name=train_rvq include_task=$include_task device=cpu
--- test_vqbet_cpu.sh
+++ test_vqbet_cpu.sh
@@ -2,5 +2,8 @@
 
 include_task="Door_Opening"
 
+wandb=true  # Set the wandb configuration here
+
 cd imitation-in-homes
-WANDB_MODE=disabled python train.py --config-name=train_vqbet include_task=$include_task vqvae_load_dir=null device=cpu
+
+WANDB_MODE=$( [ "$wandb" = true ] && echo "online" || echo "disabled" ) python train.py --config-name=train_vqbet include_task=$include_task vqvae_load_dir=null device=cpu
